bad9cb4fbfc7ba1cd310e32ab6ef33e9b32e42a2
Esther <esthertrandev@gmail.com>
2026-06-15 15:39:25 -0600

    chore: update graph structure, add orchestrator

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from langgraph.graph import END, START, StateGraph
 
 
+from graph_routers import orchestrator, profile_router
 from states.graph_states import UserState
 from states.profile_states import ProfileDraftEvent, ProfileDraftPayload
 from user_profile.profile_parser import profile_parser
 
 graph_builder = StateGraph(UserState)
 
+graph_builder.add_node("orchestrator", orchestrator)
+graph_builder.add_node("profile_router", profile_router)
 graph_builder.add_node("profile_parser", profile_parser)
 
-graph_builder.add_edge(START, "profile_parser")
+# Starting point
+graph_builder.add_edge(START, "orchestrator")
+
+# Profile route
 graph_builder.add_edge("profile_parser", END)
 
+
+# Compile graph
 graph = graph_builder.compile()
 
+
+# =============== Example ============================
 text = """
 I'm 25 years old and I'm a software engineer,
 I have 3 years working experience,
