6a570f5507efa518167a4f90465c12a623d7c924
Esther <esthertrandev@gmail.com>
2026-08-07 18:15:49 -0600

    feat: add create profile node

--- graph/nodes.py
+++ graph/nodes.py
@@ -1,5 +1,5 @@
 from graph.state.profile import ProfileDraft
-from graph.state.shared import MainState
+from graph.state.shared import MainState, UserProfile
 from user_profile.service import ProfileService
 
 
@@ -9,3 +9,10 @@ def make_profile_parser(profile_service: ProfileService):
         return profile_service.parse(profile_text=state.event.payload.text)
 
     return profile_parser
+
+
+def make_create_profile(profile_service: ProfileService):
+    def create_profile(state: MainState) -> MainState:
+        return {"profile": profile_service.create(profile=state.event.payload)}
+
+    return create_profile
