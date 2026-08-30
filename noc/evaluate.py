0132ffc37de6d4ef8f4c2eca049d504901697649
Esther <esthertrandev@gmail.com>
2026-08-29 23:46:42 -0600

    feat: add noc eval set

--- noc/evaluate.py
+++ noc/evaluate.py
@@ -1,11 +1,11 @@
 # NDCG metric to evaluate NOC retrieval method (Vector, BM25 and Hybrid)
-
 from math import log
 import os
 from db.models import NOC
 from db.service import DatabaseService
 from noc.repository import IdealNOC, NOCRepository
 from noc.service import NOCService
+import pandas as pd
 
 
 class EvaluateService:
@@ -34,7 +34,7 @@ class EvaluateService:
         for rank, relevance in enumerate(relevance_list, start=1):
             dcg_score += relevance / log(rank + 1, 2)
 
-        minor_count, major_count = noc_service.get_ideal_pool_count(ideal)
+        minor_count, major_count = self.noc_service.get_ideal_pool_count(ideal)
         ideal_relevance_list = ([3] * 1 + [2] * minor_count + [1] * major_count)[:k]
 
         # add padding zeros if necessary
@@ -48,29 +48,29 @@ class EvaluateService:
 
         return dcg_score / idcg_score
 
+if __name__ =="__main__":
+    db_service = DatabaseService(db_url=os.getenv("DB_URL"))
+    noc_repository = NOCRepository(db_service=db_service)
+    noc_service = NOCService(noc_repository=noc_repository)
+    evaluate_service = EvaluateService(noc_service=noc_service)
 
-# Example test case
-job_title = "Software Developer"
-job_responsibility = (
-    "Design, build and maintain backend REST APIs in Python, write unit "
-    "tests, review code, and deploy services to AWS."
-)
-ideal_noc = IdealNOC("21232", "2123", "21")  # Software developers and programmers
+    df = pd.read_csv("data/noc_eval_labels.csv")
+    df["search_query"] = df.apply(lambda row: f"Job title: {row['job_title']} \n Job responsibility: {row['job_responsibility']}", axis=1)
+    for row in df.itertuples():
+        search_query = row.search_query
+        noc_code = str(row.noc_code) 
+        minor_group_code = str(row.minor_group_code)
+        major_group_code = str(row.major_group_code)
+        ideal_noc = IdealNOC(noc_code=noc_code, minor_group_code=minor_group_code, major_group_code=major_group_code)
+        keyword_result = noc_service.noc_keyword_search(search_query)
+        vector_result = noc_service.noc_semantic_search(search_query)
+        hybrid_result = noc_service.noc_hybrid_search(search_query)
+        print("==============================================")
+        print(f"Case type: {row.case_type}")
+        print(f"Job title: {row.job_title}")
+        print(f"BM25: {evaluate_service.ndcg_evaluate(keyword_result, ideal_noc, 10)}")
+        print(f"Vector: {evaluate_service.ndcg_evaluate(vector_result, ideal_noc, 10)}")
+        print(f"Hybrid Search: {evaluate_service.ndcg_evaluate(hybrid_result, ideal_noc, 10)}")
+        print("==============================================")
 
-# construct search query
-search_query = f"""
-Job title: {job_title} \n Job responsibility: {job_responsibility}
-"""
-
-db_service = DatabaseService(db_url=os.getenv("DB_URL"))
-noc_repository = NOCRepository(db_service=db_service)
-noc_service = NOCService(noc_repository=noc_repository)
-evaluate_service = EvaluateService(noc_service=noc_service)
-
-keyword_result = noc_service.noc_keyword_search(search_query)
-vector_result = noc_service.noc_semantic_search(search_query)
-hybrid_result = noc_service.noc_hybrid_search(search_query)
-
-print(f"BM25: {evaluate_service.ndcg_evaluate(keyword_result, ideal_noc, 10)}")
-print(f"Vector: {evaluate_service.ndcg_evaluate(vector_result, ideal_noc, 10)}")
-print(f"Hybrid Search: {evaluate_service.ndcg_evaluate(hybrid_result, ideal_noc, 10)}")
+        
